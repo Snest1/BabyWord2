2ec42ff1d1a0b70730c797502a3ad1acf3b64f35
SNest <snest@mail.ru>
2019-02-10 15:50:32 +0300

    fff

--- BabyWords2/ASMathViewController.swift
+++ BabyWords2/ASMathViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 import AVKit
+import Foundation
 
 class ASMathViewController: UIViewController {
     @IBOutlet weak var MathButton: UIButton!
     
+    var size:Int = 3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            MathButton.setTitle(String(Int.random(in: 0..<1000)), for: .normal)
+        MathButton.setTitle(String(Int.random(in: Int(pow(Double(10),Double(size-1)))..<Int(pow(Double(10),Double(size))))), for: .normal)
         // Do any additional setup after loading the view.
     }
     
     @IBAction func HomeButton(_ sender: UIButton) {
-            dismiss(animated: true, completion: nil)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func MoreButton(_ sender: UIButton) {
+        if (size < 7) {
+            size = size + 1
+            MathButton.setTitle(String(Int.random(in: Int(pow(Double(10),Double(size-1)))..<Int(pow(Double(10),Double(size))))), for: .normal)
+        }
+    }
+    
+    @IBAction func LessButton(_ sender: UIButton) {
+        if (size > 1) {
+            size = size - 1
+            MathButton.setTitle(String(Int.random(in: Int(pow(Double(10),Double(size-1)))..<Int(pow(Double(10),Double(size))))), for: .normal)
+            
+        }
     }
     
     @IBAction func MathButton(_ sender: UIButton) {
@@ -30,16 +48,19 @@ class ASMathViewController: UIViewController {
         
     }
     
+    @IBAction func RefreshButton(_ sender: UIButton) {
+        MathButton.setTitle(String(Int.random(in: Int(pow(Double(10),Double(size-1)))..<Int(pow(Double(10),Double(size))))), for: .normal)
+    }
     
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
